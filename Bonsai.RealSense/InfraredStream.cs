03973c29772cc4582ecf81bbcd037bfc9819dbc2
glopes <glopesdev@users.noreply.github.com>
2017-02-16 23:34:49 +0000

    Added support for infrared stream.

---
+++ Bonsai.RealSense/InfraredStream.cs
@@ -0,0 +1,29 @@
+﻿using OpenCV.Net;
+using RealSense.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.RealSense
+{
+    public class InfraredStream : Combinator<DeviceEvents, IplImage>
+    {
+        public override IObservable<IplImage> Process(IObservable<DeviceEvents> source)
+        {
+            return source.SelectMany(device => Observable.FromEvent<FrameCallback, Frame>(
+                handler => device.InfraredFrame += handler,
+                handler => device.InfraredFrame -= handler))
+                .Select(frame =>
+                {
+                    var size = new Size(frame.Width, frame.Height);
+                    var image = new IplImage(size, IplDepth.U8, 1);
+                    var frameHeader = new Mat(size, Depth.U8, 1, frame.FrameData, frame.Stride);
+                    CV.Copy(frameHeader, image);
+                    return image;
+                });
+        }
+    }
+}
